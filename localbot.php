cac5609ebb37f22ed3175288a56a5e065921e336
Andrew Sorensen <andrew@localcoast.net>
2013-03-30 12:03:46 -0700

    Fix newlines, code formatting

--- localbot.php
+++ localbot.php
@@ -7,14 +7,15 @@
  */
  
 if (!file_exists('config.php')) {
-  die("The file 'config.php' is missing, please make sure it exists.");
+    die("The file 'config.php' is missing, please make sure it exists." . PHP_EOL);
 }
 
 $config = array();
 require_once "config.php";
 
 if (!file_exists('lib/localbot.php')) {
-  die("The file 'localbot.php' in lib/ is missing, please check your installation.");
+    die("The file 'localbot.php' in lib/ is missing, please check your " 
+          . "installation."  . PHP_EOL);
 }
 
 require_once "lib/LocalBot.php";
@@ -26,17 +27,15 @@ echo "[LocalBot: Loaded all plugins in " . round(
         microtime(true) - $start_time, 3) . " seconds.]" . PHP_EOL;
 
 
-while($localbot->shouldReconnect())
-{
-  if (!$localbot->connect()) {
-    die("LocalBot was unable to connect, exiting.\n");
-  }
-  sleep(3);
-  $localbot->listen(); // run this as long as were connected
-  if (!$localbot->shouldReconnect())
-  {
-    die("Exiting.\n");
-  }
-  echo "Please stand by while LocalBot is restarts.\n";
-  sleep(10);
+while ($localbot->shouldReconnect()) {
+    if (!$localbot->connect()) {
+        die("LocalBot was unable to connect, exiting." . PHP_EOL);
+    }
+    sleep(3);
+    $localbot->listen(); // run this as long as were connected
+    if (!$localbot->shouldReconnect()) {
+        die("Exiting.\n");
+    }
+    echo "Please stand by while LocalBot is restarts." . PHP_EOL;
+    sleep(10);
 }
