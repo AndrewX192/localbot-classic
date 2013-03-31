ec25f74bd75807aa9bd1d49e0b900b3f75f34db1
Andrew Sorensen <andrew@localcoast.net>
2013-03-31 11:20:46 -0700

    Add opers config

---
+++ opers.conf.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * LocalBot's opers configuration file
+ */
+$opers = array(
+    'AndrewX192' =>
+    array(
+        'hosts' => 'localhost',
+        'privs' => 'identify,
+	      oper:module,
+	      oper:ignore,
+	      oper:defcon,
+	      oper:module,
+	      oper:inject,
+	      oper:general,
+	      oper:shutdown,
+	      admin',
+    )
+);
